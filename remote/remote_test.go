ed6fb96060ae23cfe08d22ddc699a0b190b976e2
p_caiwfeng <p_caiwfeng@tencent.com>
2021-09-12 22:05:54 +0800

    add testcase for supporting udp protocol

--- remote/remote_test.go
+++ remote/remote_test.go
@@ -218,3 +218,71 @@ func TestGetTopController(t *testing.T) {
 	fmt.Println(controller.Name)
 	fmt.Println(controller.Scale)
 }
+
+func TestDdp(t *testing.T) {
+	go func() {
+		server()
+	}()
+	time.Sleep(time.Second * 5)
+	client()
+}
+
+func client() {
+	socket, err := net.DialUDP("udp4", nil, &net.UDPAddr{
+		IP:   net.IPv4(172, 20, 225, 47),
+		Port: 55555,
+	})
+	if err != nil {
+		fmt.Println("连接失败!", err)
+		return
+	}
+	defer socket.Close()
+
+	// 发送数据
+	senddata := []byte("hello server!")
+	_, err = socket.Write(senddata)
+	if err != nil {
+		fmt.Println("发送数据失败!", err)
+		return
+	}
+
+	// 接收数据
+	data := make([]byte, 4096)
+	read, remoteAddr, err := socket.ReadFromUDP(data)
+	if err != nil {
+		fmt.Println("读取数据失败!", err)
+		return
+	}
+	fmt.Println(read, remoteAddr)
+	fmt.Printf("%s\n", data[0:read])
+}
+
+func server() {
+	// 创建监听
+	socket, err := net.ListenUDP("udp4", &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 55555,
+	})
+	if err != nil {
+		return
+	}
+	defer socket.Close()
+
+	for {
+		data := make([]byte, 4096)
+		read, remoteAddr, err := socket.ReadFromUDP(data)
+		if err != nil {
+			fmt.Println("读取数据失败!", err)
+			continue
+		}
+		fmt.Println(read, remoteAddr)
+		fmt.Printf("%s\n\n", data[0:read])
+
+		senddata := []byte("hello client!")
+		_, err = socket.WriteToUDP(senddata, remoteAddr)
+		if err != nil {
+			fmt.Println("发送数据失败!", err)
+			return
+		}
+	}
+}
