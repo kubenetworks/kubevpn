c3df924e875802cb0241d06aaae4f3d0f1b14f83
p_caiwfeng <p_caiwfeng@tencent.com>
2021-10-11 10:23:27 +0800

    macOS dns support format like service.namespace:port

--- dns/dnsserver_test.go
+++ dns/dnsserver_test.go
@@ -1,14 +1,16 @@
 package dns
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"github.com/wencaiwulue/kubevpn/util"
 	"strconv"
 	"testing"
 )
 
 func TestName(t *testing.T) {
-	//port := util.GetAvailableUDPPortOrDie()
-	port := 58477
+	port := util.GetAvailableUDPPortOrDie()
+	fmt.Println(port)
 	err := NewDNSServer("udp", "127.0.0.1:"+strconv.Itoa(port), "172.20.135.131:53", "test")
 	if err != nil {
 		log.Warnln(err)
