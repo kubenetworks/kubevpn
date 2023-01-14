438509ffefe23d837f4b7eeeb608d32655342fda
fengcaiwen <fengcaiwen@bytedance.com>
2023-01-14 17:10:34 +0800

    feat: support env KUBECONFIG finally

--- pkg/util/elevatecheck_others.go
+++ pkg/util/elevatecheck_others.go
@@ -5,15 +5,19 @@ package util
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"os/exec"
 	"os/signal"
 	"runtime"
+	"strings"
 	"syscall"
 
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+const envStartSudoKubeVPNByKubeVPN = "DEPTH_SIGNED_BY_NAISON"
 
 func RunWithElevated() {
 	// fix if startup with normal user, after elevated home dir will change to root user in linux
@@ -23,11 +27,12 @@ func RunWithElevated() {
 			os.Args = append(os.Args, "--kubeconfig", clientcmd.RecommendedHomeFile)
 		}
 	}
-	cmd := exec.Command("sudo", os.Args...)
+	cmd := exec.Command("sudo", append([]string{"--preserve-env"}, os.Args...)...)
 	log.Debug(cmd.Args)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
+	cmd.Env = append(os.Environ(), envStartSudoKubeVPNByKubeVPN+"=1")
 	// while send single CTRL+C, command will quit immediately, but output will cut off and print util quit final
 	// so, mute single CTRL+C, let inner command handle single only
 	go func() {
@@ -42,5 +47,13 @@ func RunWithElevated() {
 }
 
 func IsAdmin() bool {
+	_, ok := os.LookupEnv(envStartSudoKubeVPNByKubeVPN)
+	if os.Getuid() == 0 {
+		if !ok {
+			s := "Error: Please do not use sudo to execute command kubevpn, try to use command: \n" + strings.Join(os.Args, " ")
+			fmt.Println(s)
+			os.Exit(1)
+		}
+	}
 	return os.Getuid() == 0
 }
