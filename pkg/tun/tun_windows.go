e6210836bec0b80ae61da0b0eb6493444df22521
naison <895703375@qq.com>
2024-01-18 02:20:33 +0000

    feat: add reference doc

--- pkg/tun/tun_windows.go
+++ pkg/tun/tun_windows.go
@@ -182,6 +182,20 @@ func (c *winTunConn) SetWriteDeadline(time.Time) error {
 
 /*
 *
+Reference: https://learn.microsoft.com/en-us/troubleshoot/windows-server/networking/configure-ipv6-in-windows#use-registry-key-to-configure-ipv6
+
+| IPv6 Functionality                                 | Registry value and comments                                                                   |
+|----------------------------------------------------|-----------------------------------------------------------------------------------------------|
+| Prefer IPv4 over IPv6                              | Decimal 32<br>Hexadecimal 0x20<br>Binary xx1x xxxx<br>Recommended instead of disabling IPv6.  |
+| Disable IPv6                                       | Decimal 255<br>Hexadecimal 0xFF<br>Binary 1111 1111<br>										 |
+| Disable IPv6 on all nontunnel interfaces           | Decimal 16<br>Hexadecimal 0x10<br>Binary xxx1 xxxx                                            |
+| Disable IPv6 on all tunnel interfaces              | Decimal 1<br>Hexadecimal 0x01<br>Binary xxxx xxx1                                             |
+| Disable IPv6 on all nontunnel interfaces (except the loopback) and on IPv6 tunnel interface | Decimal 17<br>Hexadecimal 0x11<br>Binary xxx1 xxx1   |
+| Prefer IPv6 over IPv4                              | Binary xx0x xxxx                                                                              |
+| Re-enable IPv6 on all nontunnel interfaces         | Binary xxx0 xxxx                                                                              |
+| Re-enable IPv6 on all tunnel interfaces            | Binary xxx xxx0                                                                               |
+| Re-enable IPv6 on nontunnel interfaces and on IPv6 tunnel interfaces | Binary xxx0 xxx0                                                            |
+
 Enable IPv6:
 
 	  Default Value 		Hexadecimal 0x00
