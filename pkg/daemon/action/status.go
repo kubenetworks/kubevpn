eb86a194669467067de6538e649159d4de61ae38
naison <895703375@qq.com>
2024-04-25 02:46:18 +0000

    hotfix: fix cmd status not correct when tun is gone

--- pkg/daemon/action/status.go
+++ pkg/daemon/action/status.go
@@ -10,20 +10,37 @@ import (
 	"github.com/wencaiwulue/kubevpn/v2/pkg/util"
 )
 
+const (
+	StatusOk     = "Connected"
+	StatusFailed = "Disconnected"
+)
+
 func (svr *Server) Status(ctx context.Context, request *rpc.StatusRequest) (*rpc.StatusResponse, error) {
 	var sb = new(bytes.Buffer)
 	w := tabwriter.NewWriter(sb, 1, 1, 1, ' ', 0)
 	_, _ = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n", "ID", "Mode", "Cluster", "Kubeconfig", "Namespace", "Status")
 	if svr.connect != nil {
-		status := "Connected"
 		cluster := util.GetKubeconfigCluster(svr.connect.GetFactory())
 		namespace := svr.connect.Namespace
 		kubeconfig := svr.connect.OriginKubeconfigPath
-		_, _ = fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%s\t%s\n", 0, "full", cluster, kubeconfig, namespace, status)
+		name, _ := svr.connect.GetTunDeviceName()
+		status := StatusOk
+		if name == "" {
+			status = StatusFailed
+		}
+		_, _ = fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%s\t%s\t%s\n",
+			0, "full", cluster, kubeconfig, namespace, status, name)
 	}
 
 	for i, options := range svr.secondaryConnect {
-		_, _ = fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%s\t%s\n", i+1, "lite", util.GetKubeconfigCluster(options.GetFactory()), options.OriginKubeconfigPath, options.Namespace, "Connected")
+		cluster := util.GetKubeconfigCluster(options.GetFactory())
+		name, _ := options.GetTunDeviceName()
+		status := StatusOk
+		if name == "" {
+			status = StatusFailed
+		}
+		_, _ = fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%s\t%s\t%s\n",
+			i+1, "lite", cluster, options.OriginKubeconfigPath, options.Namespace, status, name)
 	}
 	_ = w.Flush()
 	return &rpc.StatusResponse{Message: sb.String()}, nil
