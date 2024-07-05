3a376194428e2c88747f1a3a3dcb5829b08c6ebd
fengcaiwen <895703375@qq.com>
2024-07-05 21:52:54 +0800

    hotfix: fix log rotate

--- pkg/daemon/action/logs.go
+++ pkg/daemon/action/logs.go
@@ -1,6 +1,9 @@
 package action
 
 import (
+	"io"
+	"log"
+
 	"github.com/hpcloud/tail"
 
 	"github.com/wencaiwulue/kubevpn/v2/pkg/daemon/rpc"
@@ -8,7 +11,7 @@ import (
 
 func (svr *Server) Logs(req *rpc.LogRequest, resp rpc.Daemon_LogsServer) error {
 	path := GetDaemonLogPath()
-	config := tail.Config{Follow: req.Follow, ReOpen: true, MustExist: true}
+	config := tail.Config{Follow: req.Follow, ReOpen: false, MustExist: true, Logger: log.New(io.Discard, "", log.LstdFlags)}
 	if !req.Follow {
 		// FATAL -- cannot set ReOpen without Follow.
 		config.ReOpen = false
