8e758cbffa0b17e6cc4c2804bceff7e0c5c541ae
naison <895703375@qq.com>
2024-10-29 10:41:15 +0000

    feat: add number of lines to logs

--- pkg/daemon/action/logs.go
+++ pkg/daemon/action/logs.go
@@ -1,8 +1,10 @@
 package action
 
 import (
+	"bufio"
 	"io"
 	"log"
+	"os"
 
 	"github.com/hpcloud/tail"
 
@@ -11,6 +13,15 @@ import (
 
 func (svr *Server) Logs(req *rpc.LogRequest, resp rpc.Daemon_LogsServer) error {
 	path := GetDaemonLogPath()
+
+	lines, err2 := countLines(path)
+	if err2 != nil {
+		return err2
+	}
+
+	// only show latest N lines
+	lines -= req.Lines
+
 	config := tail.Config{Follow: req.Follow, ReOpen: false, MustExist: true, Logger: log.New(io.Discard, "", log.LstdFlags)}
 	if !req.Follow {
 		// FATAL -- cannot set ReOpen without Follow.
@@ -32,10 +43,36 @@ func (svr *Server) Logs(req *rpc.LogRequest, resp rpc.Daemon_LogsServer) error {
 			if line.Err != nil {
 				return err
 			}
+
+			if lines--; lines >= 0 {
+				continue
+			}
+
 			err = resp.Send(&rpc.LogResponse{Message: line.Text})
 			if err != nil {
 				return err
 			}
 		}
 	}
+}
+
+func countLines(filename string) (int32, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineCount := int32(0)
+
+	for scanner.Scan() {
+		lineCount++
+	}
+
+	if err = scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return lineCount, nil
 }
