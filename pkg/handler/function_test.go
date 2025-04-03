1767aba5df42240728aba98c29c2ab3a0a2f6fa2
naison <895703375@qq.com>
2025-04-03 14:46:28 +0000

    ut

--- pkg/handler/function_test.go
+++ pkg/handler/function_test.go
@@ -129,7 +129,7 @@ func healthChecker(t *testing.T, endpoint string, header map[string]string, keyw
 	}
 
 	err = retry.OnError(
-		wait.Backoff{Duration: time.Second, Factor: 1, Jitter: 0, Steps: 60},
+		wait.Backoff{Duration: time.Second, Factor: 1, Jitter: 0, Steps: 120},
 		func(err error) bool { return err != nil },
 		func() error {
 			var resp *http.Response
