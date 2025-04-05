6aa15b8b7ec2bf713c4d184e4577cb7a84071fa9
fengcaiwen <895703375@qq.com>
2025-04-05 10:38:17 +0800

    refactor: add route tun

--- pkg/core/tunhandler.go
+++ pkg/core/tunhandler.go
@@ -322,7 +322,7 @@ func (p *Peer) readFromTCPConn() {
 func (p *Peer) routeTCP() {
 	defer util.HandleCrash()
 	for packet := range p.tcpInbound {
-		if routeToAddr := p.routeMapUDP.RouteTo(packet.dst); routeToAddr != nil {
+		/*if routeToAddr := p.routeMapUDP.RouteTo(packet.dst); routeToAddr != nil {
 			plog.G(context.Background()).Debugf("[TCP] Find UDP route to dst: %s -> %s", packet.dst, routeToAddr)
 			_, err := p.conn.WriteTo(packet.data[:packet.length], routeToAddr)
 			config.LPool.Put(packet.data[:])
@@ -330,7 +330,7 @@ func (p *Peer) routeTCP() {
 				p.sendErr(err)
 				return
 			}
-		} else if conn, ok := p.routeMapTCP.Load(packet.dst.String()); ok {
+		} else*/if conn, ok := p.routeMapTCP.Load(packet.dst.String()); ok {
 			plog.G(context.Background()).Debugf("[TCP] Find TCP route SRC: %s to DST: %s -> %s", packet.src.String(), packet.dst.String(), conn.(net.Conn).RemoteAddr())
 			dgram := newDatagramPacket(packet.data[:packet.length])
 			err := dgram.Write(conn.(net.Conn))
@@ -361,6 +361,16 @@ func (p *Peer) routeTUN() {
 			config.LPool.Put(packet.data[:])
 			if err != nil {
 				plog.G(context.Background()).Debugf("[TUN] Failed wirte to route dst: %s -> %s", packet.dst, addr)
+				p.sendErr(err)
+				return
+			}
+		} else if conn, ok := p.routeMapTCP.Load(packet.dst.String()); ok {
+			plog.G(context.Background()).Debugf("[TUN] Find TCP route to dst: %s -> %s", packet.dst.String(), conn.(net.Conn).RemoteAddr())
+			dgram := newDatagramPacket(packet.data[:packet.length])
+			err := dgram.Write(conn.(net.Conn))
+			config.LPool.Put(packet.data[:])
+			if err != nil {
+				plog.G(context.Background()).Errorf("[TUN] Failed to write TCP %s <- %s : %s", conn.(net.Conn).RemoteAddr(), dgram.Addr(), err)
 				p.sendErr(err)
 				return
 			}
