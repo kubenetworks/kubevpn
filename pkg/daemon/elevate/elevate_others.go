c01316aa8613cec3d99de2eb70e021a7dbb3409d
mazhong <mazhong@kingsoft.com>
2025-06-18 16:37:49 +0800

    fix:linux startup must use sudo

--- pkg/daemon/elevate/elevate_others.go
+++ pkg/daemon/elevate/elevate_others.go
@@ -4,20 +4,14 @@ package elevate
 
 import (
 	"context"
-	plog "github.com/wencaiwulue/kubevpn/v2/pkg/log"
-	"golang.org/x/sys/unix"
 	"os"
 	"os/exec"
+
+	plog "github.com/wencaiwulue/kubevpn/v2/pkg/log"
+	"golang.org/x/sys/unix"
 )
 
 func RunCmdWithElevated(exe string, args []string) error {
-	// fix if startup with normal user, after elevated home dir will change to root user in linux
-	// but unix don't have this issue
-	//if runtime.GOOS == "linux" && flag.Lookup("kubeconfig") == nil {
-	//	if _, err := os.Stat(clientcmd.RecommendedHomeFile); err == nil {
-	//		os.Args = append(os.Args, "--kubeconfig", clientcmd.RecommendedHomeFile)
-	//	}
-	//}
 	cmd := exec.Command("sudo", append([]string{"--preserve-env=HOME", "--background", exe}, args...)...)
 	plog.G(context.Background()).Debug(cmd.Args)
 	cmd.Stdout = os.Stdout
@@ -36,13 +30,6 @@ func RunCmdWithElevated(exe string, args []string) error {
 }
 
 func RunCmd(exe string, args []string) error {
-	// fix if startup with normal user, after elevated home dir will change to root user in linux
-	// but unix don't have this issue
-	//if runtime.GOOS == "linux" && flag.Lookup("kubeconfig") == nil {
-	//	if _, err := os.Stat(clientcmd.RecommendedHomeFile); err == nil {
-	//		os.Args = append(os.Args, "--kubeconfig", clientcmd.RecommendedHomeFile)
-	//	}
-	//}
 	cmd := exec.Command(exe, args...)
 	cmd.SysProcAttr = &unix.SysProcAttr{
 		Setpgid: true,
