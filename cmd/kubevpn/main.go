a8a37616db9459ca8a15f292db3b422b84be7fe5
Pierluigi Lenoci <pierluigilenoci@gmail.com>
2026-04-24 14:24:44 +0200

    Honor stderrthreshold when logtostderr is enabled
    Bump k8s.io/klog/v2 to v2.140.0 and opt out of the legacy stderr
    threshold behavior so that -stderrthreshold is respected even when
    -logtostderr=true (the default).  Set stderrthreshold=INFO to preserve
    current behavior; users can now override it on the command line.
    
    Ref: kubernetes/klog#212, kubernetes/klog#432
    Signed-off-by: Pierluigi Lenoci <pierluigilenoci@gmail.com>

--- cmd/kubevpn/main.go
+++ cmd/kubevpn/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"flag"
 	_ "net/http/pprof"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/klog/v2"
 
 	"github.com/wencaiwulue/kubevpn/v2/cmd/kubevpn/cmds"
 )
 
 func main() {
+	// Register klog flags on the standard flag set so that they can be
+	// configured programmatically.  klog v2 defaults -logtostderr to true,
+	// which silently ignores -stderrthreshold.  Opt out of that legacy
+	// behaviour (kubernetes/klog#432) so users can control the threshold.
+	klog.InitFlags(nil)
+	flag.Set("legacy_stderr_threshold_behavior", "false") //nolint:errcheck
+	flag.Set("stderrthreshold", "INFO")                   //nolint:errcheck
+
 	ctx := ctrl.SetupSignalHandler()
 	_ = cmds.NewKubeVPNCommand().ExecuteContext(ctx)
 }
